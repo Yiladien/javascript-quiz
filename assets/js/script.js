6163c038a0802d9a28eda657f0d3ceb29304318d
yiladien@hotmail.com <yiladien@hotmail.com>
2022-08-10 02:44:50 -0500

    Updated DOM with quiz and timer ends game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,30 @@
 var questionCounter = 0;
 var questionUsed = [];
+var questionNumber = 0;
+var score = 0;
 
 // Timer
 var timerEl = document.getElementById("countdown");
+var timeLeft = 10;
 
 var quizQuestionEl = document.getElementById("quiz-question");
 var quizParagraphEl = document.getElementById("quiz-paragraph");
 var quizChoicesEl = document.getElementById("quiz-choices");
+var quizAnswersEl = document.getElementById("answer-list");
 var quizStartBtnEl = document.getElementById("start");
 var quizHighScoreBtnEl = document.getElementById("high-score-btn");
 var quizFeedbackEl = document.getElementById("quiz-feedback");
 
-console.log(quizQuestionEl);
+console.log(quizChoicesEl);
 
 //countdown timer
 function countdown() {
-  var timeLeft = 10;
-
+  timerEl.textContent = "Time: " + timeLeft + "s";
+  timeLeft--;
   var timeInterval = setInterval(function () {
-    if (timeLeft === 0) {
+    if (timeLeft <= 0) {
       clearInterval(timeInterval);
+      clearQuestions();
     }
     timerEl.textContent = "Time: " + timeLeft + "s";
     timeLeft--;
@@ -28,68 +33,14 @@ function countdown() {
 
 var quizStart = function (event) {
   console.log("quizStart");
-  console.log(quizQuestionEl);
   countdown();
 
-  // Creating array to track questions used
-  var questionNumberList = [];
-  for (var i = 0; i < quizQuestions.length; i++) {
-    questionNumberList[i] = i;
-  }
-
-  currentQuestion = getQuestion(questionNumberList);
-
-  displayQuestion(currentQuestion);
+  getQuestion();
 };
 
-function displayQuestion(currentQuestion) {
-  quizStartBtnEl.remove();
-  console.log(quizQuestionEl.value);
-  console.log(quizQuestions[currentQuestion].question);
-  quizQuestionEl.textContent = quizQuestions[currentQuestion].question;
-  quizQuestionEl.setAttribute("data-question", currentQuestion);
-  quizQuestionEl.setAttribute("class", "question");
-
-  quizParagraphEl.textContent = "";
-
-  for (var i = 0; i < quizQuestions[currentQuestion].choices.length; i++) {
-    var buttonContainerEl = document.createElement("li");
-
-    var questionButtonEl = document.createElement("button");
-    questionButtonEl.textContent = quizQuestions[currentQuestion].choices[i];
-    questionButtonEl.className = "btn quiz-btn";
-    questionButtonEl.setAttribute("type", "button");
-    questionButtonEl.setAttribute(
-      "data-response",
-      quizQuestions[currentQuestion].choices[i]
-    );
-    buttonContainerEl.appendChild(questionButtonEl);
-    quizChoicesEl.appendChild(buttonContainerEl);
-  }
-
-  quizFeedbackEl.className = "feedbackSection";
-}
-
-function getQuestion(questionNumberList) {
-  // NO REPEAT QUESTIONS METHOD 1
-  //     var newQuestion = false
-  //     while(newQuestion = false) {
-  //     var questionNumber = Math.floor(Math.random() * quizQuestions.length);
-  //   console.log(questionNumber);
-
-  //   for (var i = 0; i < questionUsed.length, i++) {
-
-  //     if (questionNumber === questionUsed[i]) {
-  //         newQuestion = false;
-  //         i=questionUsed.length;
-  //     } else {
-  //         newQuestion = true;
-  //     }
-  //   }
-
-  // NO REPEAT QUESTIONS METHOD 2
+function getQuestion() {
   // selects random number to match the position in array based on size of array
-  var questionNumber = Math.floor(Math.random() * questionNumberList.length);
+  questionNumber = Math.floor(Math.random() * questionNumberList.length);
   // creates temporary array to hold new array after removing the random number from the list
   var updatedQuestionNumberList = [];
 
@@ -102,9 +53,96 @@ function getQuestion(questionNumberList) {
 
   // updates array after removing the question to be used
   questionNumberList = updatedQuestionNumberList;
+  console.log(updatedQuestionNumberList);
+  console.log(questionNumberList);
 
   // provides number to correspond to array position in question object array
-  return (currentQuestion = questionNumber);
+  displayQuestion();
+}
+
+function displayQuestion() {
+  quizStartBtnEl.remove();
+  quizQuestionEl.textContent = quizQuestions[questionNumber].question;
+  quizQuestionEl.setAttribute("data-question", questionNumber);
+  quizQuestionEl.setAttribute("class", "question");
+
+  quizParagraphEl.textContent = "";
+
+  var quizChoicesEl = document.createElement("ul");
+  quizChoicesEl.id = "quiz-choices";
+
+  for (var i = 0; i < quizQuestions[questionNumber].choices.length; i++) {
+    var buttonContainerEl = document.createElement("li");
+
+    var questionButtonEl = document.createElement("button");
+    questionButtonEl.textContent = quizQuestions[questionNumber].choices[i];
+    questionButtonEl.className = "btn quiz-btn";
+    questionButtonEl.setAttribute("type", "button");
+    questionButtonEl.setAttribute(
+      "data-response",
+      quizQuestions[questionNumber].choices[i]
+    );
+    buttonContainerEl.appendChild(questionButtonEl);
+    quizChoicesEl.appendChild(buttonContainerEl);
+    quizAnswersEl.appendChild(quizChoicesEl);
+  }
+
+  quizFeedbackEl.className = "feedbackSection";
+}
+
+function userAnswers(event) {
+  console.log(quizQuestions[questionNumber].answer);
+  var targetEl = event.target;
+  console.log(targetEl);
+
+  if (targetEl.matches(".quiz-btn")) {
+    var userAnswer = targetEl.getAttribute("data-response");
+    console.log(userAnswer);
+    answerTest(userAnswer);
+  } else {
+    return false;
+  }
+}
+
+function answerTest(userAnswer) {
+  if (
+    userAnswer.toLowerCase() ===
+    quizQuestions[questionNumber].answer.toLowerCase()
+  ) {
+    score++;
+    console.log(score);
+    var quizFeedbackResponseEl = document.createElement("p");
+    quizFeedbackResponseEl.textContent = "Correct!";
+    quizFeedbackResponseEl.className = "feedbackResponse";
+    quizFeedbackEl.appendChild(quizFeedbackResponseEl);
+    console.log(quizFeedbackEl);
+    var pauseTime = setTimeout(clearQuestions, 5000);
+  } else {
+    timeLeft = timeLeft - 10;
+  }
+}
+
+function clearQuestions() {
+  var quizChoicesEl = document.getElementById("quiz-choices");
+  var quizFeedbackEl = document.getElementById("quiz-feedback");
+  var quizFeedbackResponseEl = document.querySelector(".feedbackResponse");
+  console.log(quizChoicesEl);
+  quizChoicesEl.remove();
+  quizFeedbackEl.removeAttribute("class");
+  if (quizFeedbackResponseEl) {
+    quizFeedbackResponseEl.remove();
+  }
+
+  if (timeLeft <= 0) {
+    quizQuestionEl.removeAttribute("class");
+    endGame();
+  } else {
+    getQuestion();
+  }
+}
+
+function endGame() {
+  quizQuestionEl.textContent = "Time is up! Your score is " + score + "!";
 }
 
 var highScore = function (event) {
@@ -140,8 +178,17 @@ var quizQuestions = [
   },
 ];
 
+// Creating array to track questions used
+var questionNumberList = [];
+for (var i = 0; i < quizQuestions.length; i++) {
+  questionNumberList[i] = i;
+}
+
 // Start Quiz Button
 quizStartBtnEl.addEventListener("click", quizStart);
 
 // High Score Button
 quizHighScoreBtnEl.addEventListener("click", highScore);
+
+// Quiz buttons
+quizAnswersEl.addEventListener("click", userAnswers);
